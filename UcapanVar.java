5b657065e25e8ff2976f40faf1419eb44a030c9f
FauzanNizam <nzmffcl73@gmail.com>
2024-09-20 18:51:51 +0800

    Add files via upload

---
+++ UcapanVar.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tipedata;
+
+/**
+ *
+ * @author nzmff
+ */
+public class UcapanVar {
+    public static void main(String[] args) {
+        String halo = "Halo ULM";
+        String bidang = "ULM Berdaya Saing dan Unggul di Bidang Lahan Basah";
+        String prodi = "Prodi Ilkom ULM Unggul dengan Teknologi Kekinian";
+        
+        System.out.println(halo + "\n" + bidang + "\n" + prodi); 
+        
+    }
+}
